f371a583f0073144e6458fb2cce975cbfd75f5cd
longern <i@longern.com>
2024-08-30 16:33:43 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,98 @@
+import { X509Certificate } from "crypto";
+import fs from "fs";
+import net from "net";
+import { execSync } from "child_process";
+import { Http3Server } from "@fails-components/webtransport";
+
+const CREATE_CERTIFICATE_COMMAND = `\
+openssl req -new \
+-newkey ec \
+-pkeyopt ec_paramgen_curve:prime256v1 \
+-x509 \
+-nodes \
+-days 14 \
+-out ./cert.pem \
+-keyout ./key.pem \
+-subj '/CN=Test Certificate' \
+-addext 'subjectAltName = DNS:localhost'`;
+
+function createCertificate() {
+  execSync(CREATE_CERTIFICATE_COMMAND);
+
+  const cert = fs.readFileSync("./cert.pem");
+  const privKey = fs.readFileSync("./key.pem");
+
+  const hash = Buffer.from(
+    new X509Certificate(cert).fingerprint256
+      .split(":")
+      .map((el) => parseInt(el, 16))
+  );
+
+  return { cert, privKey, hash };
+}
+
+const tcpPort = process.argv[2];
+
+async function readStreamLoop(session) {
+  const tcpClient = new net.Socket();
+
+  try {
+    await new Promise((resolve, reject) => {
+      tcpClient.connect(tcpPort, resolve);
+      setTimeout(reject, 3000);
+    });
+  } catch (e) {
+    const datagramWriter = session.datagram.writable.getWriter();
+    datagramWriter.write(new TextEncoder().encode("ERR_CONNECTION_REFUSED"));
+    session.close();
+  }
+
+  try {
+    const streamReader = session.incomingBidirectionalStreams.getReader();
+    const { value: stream, done } = await streamReader.read();
+    if (done) return;
+    const writer = stream.writable.getWriter();
+    tcpClient.on("data", (data) => writer.write(data));
+    tcpClient.on("end", () => writer.close());
+
+    for await (const chunk of stream.readable) tcpClient.write(chunk);
+  } catch (e) {}
+}
+
+async function sessionStreamLoop(sessionStream) {
+  for await (const session of sessionStream) {
+    session.ready.then(() => readStreamLoop(session));
+  }
+}
+
+async function main() {
+  if (!tcpPort) {
+    console.log("Usage: node server.js <tcp-port>");
+    process.exit(1);
+  }
+
+  const { cert, privKey, hash } = createCertificate();
+  const port = 24433;
+
+  console.log();
+  console.log("URL:", `127.0.0.1:${port}`);
+  console.log("Cert hash:", hash.toString("base64"));
+
+  const server = new Http3Server({
+    port,
+    host: "0.0.0.0",
+    secret: "mysecret",
+    cert: cert,
+    privKey: privKey,
+  });
+
+  await server.createTransportInt();
+
+  const sessionStream = server.sessionStream("/");
+
+  sessionStreamLoop(sessionStream);
+
+  server.startServer();
+}
+
+main();
---
+++ sw.js
@@ -0,0 +1,160 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+/**
+ * @param {string} base64
+ */
+function base64ToArrayBuffer(base64) {
+  return Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+}
+
+/**
+ * @param {Request} request
+ * @returns ReadableStream
+ */
+function encodeHttpRequest(request) {
+  const headers = `${request.method} ${request.url} HTTP/1.1\r\n${[
+    ...request.headers,
+  ]
+    .map(([name, value]) => `${name}: ${value}`)
+    .join("\r\n")}\r\n\r\n`;
+
+  const transformer = new TransformStream({
+    start(controller) {
+      controller.enqueue(new TextEncoder().encode(headers));
+    },
+    transform(chunk, controller) {
+      controller.enqueue(chunk);
+    },
+  });
+
+  const body = request.body ? request.body : new ReadableStream();
+
+  return body.pipeThrough(transformer);
+}
+
+function timeoutWrapper(promise, timeout, message = "Timeout") {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => {
+      reject(new Error(message));
+    }, timeout);
+    promise.then(resolve, reject).finally(() => clearTimeout(timer));
+  });
+}
+
+/**
+ * @param {ReadableStream} readable
+ */
+async function decodeHttpResponse(readable) {
+  const reader = readable.getReader();
+
+  const decoder = new TextDecoder();
+  let headerChunks = "";
+  let rest = new Uint8Array();
+  while (true) {
+    const { value, done } = await timeoutWrapper(reader.read(), 5000);
+    if (done) break;
+    headerChunks += decoder.decode(value, { stream: true });
+    const emptyLineIndex = headerChunks.indexOf("\r\n\r\n");
+    if (emptyLineIndex !== -1) {
+      headerChunks = headerChunks.slice(0, emptyLineIndex);
+      rest = value.slice(emptyLineIndex + 4);
+      break;
+    }
+  }
+
+  const [statusLine, ...headerLines] = headerChunks.split("\r\n");
+  const [, status, statusText] = statusLine.match(/HTTP\/1.1 (\d+) (.+)/);
+  const headers = new Headers(
+    Object.fromEntries(headerLines.map((line) => line.split(": ", 2)))
+  );
+
+  const contentLength = parseInt(headers.get("Content-Length"));
+  let lengthRead = rest.length;
+  const body = new ReadableStream({
+    start(controller) {
+      controller.enqueue(rest);
+    },
+    async pull(controller) {
+      const { value, done } = await reader.read();
+      if (done) {
+        controller.close();
+        return;
+      }
+      controller.enqueue(value);
+      lengthRead += value.length;
+      if (lengthRead >= contentLength) controller.close();
+    },
+  });
+
+  const response = new Response(lengthRead === contentLength ? rest : body, {
+    status: parseInt(status),
+    statusText,
+    headers,
+  });
+  return response;
+}
+
+self.addEventListener("fetch", async (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  function detectUrlAndCertificate() {
+    const url = new URL(request.url);
+    const wturl = url.searchParams.get("wturl");
+    const wtsch = (url.searchParams.get("wtsch") ?? "").split(",");
+    if (wturl && wtsch.length) {
+      return {
+        url: wturl,
+        serverCertificateHashes: wtsch,
+      };
+    }
+
+    if (request.referrer) {
+      const url = new URL(request.referrer);
+      const wturl = url.searchParams.get("wturl");
+      const wtsch = (url.searchParams.get("wtsch") ?? "").split(",");
+      if (wturl && wtsch.length) {
+        return {
+          url: wturl,
+          serverCertificateHashes: wtsch,
+        };
+      }
+    }
+
+    return { url: null, serverCertificateHashes: null };
+  }
+
+  const { url: webTransportUrl, serverCertificateHashes } =
+    detectUrlAndCertificate();
+
+  if (url.origin !== self.location.origin || !webTransportUrl) return;
+
+  event.respondWith(
+    (async () => {
+      const wt = new WebTransport(`https://${webTransportUrl}`, {
+        serverCertificateHashes: serverCertificateHashes.map((hash) => ({
+          algorithm: "sha-256",
+          value: base64ToArrayBuffer(hash),
+        })),
+      });
+
+      try {
+        await timeoutWrapper(wt.ready, 10000, "WebTransport timeout");
+
+        const stream = await wt.createBidirectionalStream();
+        encodeHttpRequest(request).pipeTo(stream.writable);
+        const response = await decodeHttpResponse(stream.readable);
+        return response;
+      } catch (e) {
+        console.error(e);
+        return fetch(request);
+      }
+    })()
+  );
+});
